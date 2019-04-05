e34c71934a3572e120f5090e3bc986d6d353844c
James Trauer <james.trauer@monash.edu>
2019-04-05 15:06:09 +1100

    Move baseline model function to a new file, so that it can be sourced from multiple places

--- sensitvitiy.r
+++ sensitvitiy.r
@@ -7,53 +7,9 @@ library(lhs)
 library(sensitivity)
 
 source("function_tool_kit.R")
+source("model_functions.R")
 
 # SENSITIVITY ANALYSIS
-
-# Model Func
-Baseline_model <- function(current_timepoint, state_values, parameters)
-  {
-  # create state variables (local variables)
-  S=state_values[1] #fully susceptible 
-  L1=state_values[2] #early latency
-  L2=state_values[3] #late latency
-  I0=state_values[4] #infectious non-spreaders
-  I1=state_values[5] #infectious spreaders
-  I2=state_values[6] #infectious Super-spreaders
-  
-  with(
-    as.list(parameters), #variable names within parameters can be used
-    {
-      #compute derivative
-      dS = mu * N + mui0 * I0 + mui1 * I1 + mui2 * I2 +
-        delta0_b * I0 + delta1_b * I1 + delta2_b * I2 +
-        r * (p1 * beta1 * I1 + p2 * beta2 * I2) / N * L2 - 
-        (beta0 * I0 + (1 - p1) * beta1 * I1 + (1 - p2) * beta2 * I2) / N * S -
-        mu*S
-      dL1 = (beta0 * I0 + (1 - p1) * beta1 * I1 + (1 - p2) * beta2 * I2) / N * S +
-        r * (beta0 * I0 + (1 - p1) * beta1 * I1 + (1 - p2) * beta2 * I2) / N * L2 -
-        (epsilon0 + epsilon1 + epsilon2 + mu + kappa) * L1
-      dL2 = kappa * L1 + 
-        gamma0 * I0 + gamma1 * I1 + gamma2 * I2 -
-        r * (beta0 * I0 + beta1 * I1 + beta2 * I2) / N * L2 -
-        (nu0 + nu1 + nu2 + mu) * L2
-      dI0 = epsilon0 * L1 + nu0 * L2 -
-        (mui0 + mu + gamma0 + delta0_b + h) * I0
-      dI1 = epsilon1 * L1 + nu1 * L2 + h * I0 -
-        (mui1 + mu + gamma1 + delta1_b + j) * I1
-      dI2 = epsilon2 * L1 + nu2 * L2 + j * I1 -
-        (mui2 + mu + gamma2 + delta2_b) * I2
-      
-      dinc = epsilon0 * L1 + nu0 * L2 + 
-        epsilon1 * L1 + nu1 * L2 + epsilon2 * L1 + nu2 * L2
-      
-      #combine results
-      results = c(dS, dL1, dL2, dI0, dI1, dI2, dinc)
-      list(results)
-    }
-  )
-}
-
 
 # beginning to shift this code over to being based on lists, to avoid repeated calculations using LHS to sample from prop_I0 particular window
 param_value_limits <- list(N= list(min = 1, max = 1),
@@ -162,15 +118,11 @@ I2_init = 1e-6 * prop_I2
 for(i in 1:n_runs){
   
   #run baseline
-  initial_values = c(S=S_init-(I0_init+I1_init+I2_init),
-                     L1=L1_init,
-                     L2=L2_init,
-                     I0=I0_init,
-                     I1=I1_init,
-                     I2=I2_init,
-                     inc=I0_init + I1_init + I2_init)
+  initial_values = c(S = S_init - I0_init - I1_init - I2_init,
+                     L1 = L1_init, L2 = L2_init, I0 = I0_init, I1 = I1_init, I2 = I2_init,
+                     inc = I0_init + I1_init + I2_init)
   params <- as.list(c(output_matrix_equi[i,]))
-  times=seq(0, 10000, by = 1)
+  times <- seq(0, 10000)
   B_out <- as.data.frame(lsoda(initial_values, times, Baseline_model, params))
   Nq=B_out$S+B_out$L1+B_out$L2+B_out$I0+B_out$I1+B_out$I2
   
