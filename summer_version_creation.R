eb6f27a61a74da1a5503c6cafa94c457f634224e
James Trauer <james.trauer@monash.edu>
2019-04-25 20:36:42 +1000

    Get code running with original sensitivity.R file again

---
+++ summer_version_creation.R
@@ -0,0 +1,38 @@
+
+# specification of summer version of model
+summer_version <- EpiModel$new(times, names(initial_values), as.list(initial_values), params,
+                               list(c("infection_frequency", "beta", "S", "L1"),
+                                    c("infection_frequency", "beta_reinfection", "L2", "L1"),
+                                    c("standard_flows", "kappa", "L1", "L2"),
+                                    c("standard_flows", "epsilon", "L1", "I"),
+                                    c("standard_flows", "nu", "L2", "I"),
+                                    c("standard_flows", "gamma", "I", "L2"),
+                                    c("standard_flows", "delta", "I", "S"),
+                                    c("compartment_death", "mui0", "I")),
+                               infectious_compartment="I", initial_conditions_sum_to_total = FALSE, report_progress = FALSE, reporting_sigfigs = 6,
+                               birth_approach = "replace_deaths", entry_compartment = "S")
+
+# stratify by infectiousness and modify relevant parameters
+summer_version$stratify("infect", seq(0, 2), c("I"),
+                        list(epsilon=list(adjustments=list("0"=params$prop_I0,
+                                                           "1"=params$prop_I1,
+                                                           "2"=params$prop_I2)),
+                             nu=list(adjustments=list("0"=params$prop_I0,
+                                                      "1"=params$prop_I1,
+                                                      "2"=params$prop_I2)),
+                             gamma=list(adjustments=list("0"=params$gamma0,
+                                                         "1"=params$gamma1,
+                                                         "2"=params$gamma2),
+                                        overwrite=seq(0, 2)),
+                             mui=list(adjustments=list("0"=params$mui0,
+                                                       "1"=params$mui1,
+                                                       "2"=params$mui2),
+                                      overwrite=seq(0, 2))),
+                        infectiousness_adjustments = c("0"=params$alpha_0,
+                                                       "1"=params$alpha_1,
+                                                       "2"=1),
+                        report = FALSE)
+
+# add the between-infectious compartment flows
+summer_version$add_transition_flow(c("standard_flows", "h", "IXinfect_0", "IXinfect_1"))
+summer_version$add_transition_flow(c("standard_flows", "j", "IXinfect_1", "IXinfect_2"))
--- test_summer_translation.R
+++ test_summer_translation.R
@@ -6,6 +6,7 @@ source("summer_model.R")
 setwd("//ad.monash.edu/home/User096/jtrauer/Documents/GitHub/heterogeneity")
 # setwd("C://Users/jtrauer/Desktop/heterogeneity/heterogeneity")
 source("function_tool_kit.R")
+source("model_functions.R")
 library(lhs)
 local_repo_directory <- getwd()
 setwd(local_repo_directory)
@@ -66,8 +67,13 @@ uncertainty_params <- list(P_epsilon = c(min = 0.074, max = 0.128),
                            P_j = list(min = 0, max = 0.058),
                            P_h = list(min = 0, max = 0.058))
 
-# user to request number of runs
+# user to request number of runs, set model intial conditions and specify integration time
 n_runs <- 5
+S_init = 1
+I_init = 1e-6
+initial_values = c(S = S_init - I_init, L1 = 0, L2 = 0, I = I_init)  # summer will split the I compartment
+initial_values_yaye_version <- c(S = S_init - I_init, L1 = 0, L2 = 0, I0 = I_init / 3, I1 = I_init / 3, I2 = I_init / 3)
+times <- seq(0, 1e2)
 
 # do the LHS sampling for all parameters and all runs
 lhs_samples <- as.data.frame(maximinLHS(n_runs, length(uncertainty_params)))
@@ -80,23 +86,14 @@ for (run in seq(n_runs)) {
   print("run number")
   print(run)
 
-  # use the LHS sample values to determine parameter values
+  # use the LHS sample values to determine parameter values and adapt parameters from epidemiological values to model flows
   for (param in names(uncertainty_params)) {
     params[[param]] <- adjust_lhs_to_range(lhs_samples[[param]][run], param, uncertainty_params)
   }
-  
-  # adapt parameters from epidemiological values to model flows
   params <- process_parameters(params)
   
-  # set model intial conditions and specify integration time
-  S_init = 1
-  I_init = 1e-6
-  initial_values = c(S = S_init - I_init, L1 = 0, L2 = 0, I = I_init)
-  initial_values_yaye_version <- c(S = S_init - I_init, L1 = 0, L2 = 0, I0 = I_init / 3, I1 = I_init / 3, I2 = I_init / 3)
-  times <- seq(0, 1e2)
-  
   # create summer model and define yaye model
-  source("model_functions.R")
+  source("summer_version_creation.R")
   
   # yaye version new
   yaye_version <- as.data.frame(lsoda(initial_values_yaye_version, times, YayeModel, params))
@@ -105,9 +102,7 @@ for (run in seq(n_runs)) {
   summer_version$run_model()
   
   # print comparison of outputs
-  print("direct ode-based version, prevalence of I0")
-  print(tail(yaye_version$I0, 1) * 1e5)
-  print("summer version, prevalence of I0")
-  print(tail(summer_version$outputs$IXinfect_0, 1) * 1e5)
+  writeLines(paste("direct ode-based version, prevalence of I0:", tail(yaye_version$I0, 1) * 1e5))
+  writeLines(paste("summer interpretation, prevalence of I0:   ", tail(summer_version$outputs$IXinfect_0, 1) * 1e5))
 }
 
